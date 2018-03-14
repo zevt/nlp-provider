4f2f00393e3f835acaf3305cb5b789f8d5b3c5da
Viet Quoc Tran <viet.tran@siemens.com>
2018-03-14 10:19:24 -0500

    init tokenizer

---
+++ ze-tokenizer/src/main/java/com/zeroexception/zetokenizer/ZeTokenizerApplication.java
@@ -0,0 +1,12 @@
+package com.zeroexception.zetokenizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeTokenizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeTokenizerApplication.class, args);
+	}
+}
---
+++ ze-tokenizer/src/test/java/com/zeroexception/zetokenizer/ZeTokenizerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zeroexception.zetokenizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZeTokenizerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
